e429f1f651bb0df77ece2961319b1615452ee089
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-21 11:59:55 +1000

    set current time at hours:minutes day,month,year

--- ex3.js
+++ ex3.js
@@ -1,7 +1,54 @@
 // Write a function  ...
 
-// 1. That logs “The current date and time is “ - the current date and time 
+// 1. That logs “The current date and time is “ - the current date and time
 // hint; you will need to research the Date object to get the current date! (https://www.w3schools.com/jsref/jsref_obj_date.asp)
+
+dateTime = () => {
+  let date = new Date();
+  let year = date.getFullYear();
+
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "thursday",
+    "Friday",
+    "Saturday",
+    "Sundays",
+  ];
+  let day = days[date.getDay() - 1];
+
+  let months = [
+    "Janruary",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let currentTime = `The current time and date is: ${hours}:${minutes}, ${day} ${month} ${year}`;
+  return console.log(currentTime);
+};
+
+dateTime();
 
 // 2. That prints “I’m ready” after 5 seconds.
 // hint: you will need to research the setTimeout JavaScript function. (https://www.w3schools.com/jsref/met_win_settimeout.asp)
