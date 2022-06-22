076b9b1511e22d399ee644f5822550febaa9200a
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-22 10:15:35 +1000

    ex D and E complete

--- homework/exD.js
+++ homework/exD.js
@@ -1,3 +1,7 @@
 // Use a while loop to display the numbers between 20 and 30(inclusive).
 // Inputs: None
 // Outputs: Message: 20, 21, …,29, 30.
+
+for (let i = 20; i <= 30; i++) {
+  console.log(i);
+}
