35e61d65648f49547f70487ac28f505fee418fe2
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-25 17:16:04 +1000

    done exB

--- homework/exB.js
+++ homework/exB.js
@@ -1,4 +1,4 @@
-// The todaysGreeting() function should run three console log greetings. 
+// The todaysGreeting() function should run three console log greetings.
 // The myCounter function should count and return how many times it has been run.
 
 // The output of the file when run should be:
@@ -10,27 +10,27 @@
 // The code has 6 errors, find them and fix them and use node to check it is working.
 
 const morningGreetings = (name) => {
-    console.log(`Good morning ${name}`); 
-  }
-  const toDo = function (taskName) {
-    console.log(`Today you should ${name}`);
-  }
-   
-  const todaysGreeting = () {
-    morningGreetings('Lisa')
-    console.log(`Thanks for all you have done during yesterday`);
-    toDo('practice your JavaScript further.");
-  }
-   
-  todaysGreeting();
-   
-   function myCounter ()  {
-    let count = 0
-    return function () {
-       ++count;
-    }
-  }
-  const noOfTimes = myCounters();
-  noOfTimes();
-  console.log(noOfTimes("is this string meant to be here?")); 
-  
+  console.log(`Good morning ${name}`);
+};
+const toDo = function (taskName) {
+  console.log(`Today you should ${taskName}`);
+};
+
+const todaysGreeting = () => {
+  morningGreetings("Lisa");
+  console.log(`Thanks for all you have done during yesterday`);
+  toDo("practice your JavaScript further.");
+};
+
+todaysGreeting();
+
+function myCounter() {
+  let count = 0;
+  return function () {
+    return ++count;
+  };
+}
+const noOfTimes = myCounter();
+noOfTimes();
+
+console.log(noOfTimes());
